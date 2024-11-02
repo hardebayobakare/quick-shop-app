920fc1cd14cd6aa37567fa2cd3ba7dec965ee41a
Adebayo Bakare <hardebayobakare@gmail.com>
2024-11-02 15:14:53 -0400

    buttom navigation design

---
+++ lib/common/menu/navigation_menu.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:quick_shop_app/features/authentication/screens/home/home.dart';
+import 'package:quick_shop_app/utils/constants/colors.dart';
+import 'package:quick_shop_app/utils/helpers/helper_functions.dart';
+
+class NavigationMenu extends StatelessWidget {
+  const NavigationMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(NavigationController());
+    final dark = CustomHelperFunctions.isDarkMode(context);
+    return Scaffold(
+      bottomNavigationBar: Obx(() => 
+        NavigationBar(
+          elevation: 0,
+          selectedIndex: controller.selectedIndex.value,
+          onDestinationSelected: (index) => controller.onDestinationSelected(index),
+          backgroundColor: dark ? CustomColors.black : Colors.white,
+          indicatorColor: dark ? CustomColors.white.withOpacity(0.1) : CustomColors.black.withOpacity(0.1),
+          destinations: const [
+            NavigationDestination(icon: Icon(Iconsax.home), label: "home"),
+            NavigationDestination(icon: Icon(Iconsax.shop), label: "Store"),
+            NavigationDestination(icon: Icon(Iconsax.heart), label: "Wishlist"),
+            NavigationDestination(icon: Icon(Iconsax.user), label: "Profile"),
+          ]
+        ),
+      ),
+      body: Obx(() => controller.screens[controller.selectedIndex.value]),
+    );
+  }
+}
+
+class NavigationController extends GetxController {
+  final Rx<int> selectedIndex = 0.obs;
+  final screens = [
+    const HomeScreen(),
+    Container(color: Colors.blue),
+    Container(color: Colors.purple),
+    Container(color: Colors.orange),
+  ];
+
+
+  void onDestinationSelected(int index) {
+    selectedIndex.value = index;
+  }
+}
--- lib/features/authentication/screens/login/widget/login_form.dart
+++ lib/features/authentication/screens/login/widget/login_form.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:quick_shop_app/common/menu/navigation_menu.dart';
 import 'package:quick_shop_app/features/authentication/screens/password_configuration/forget_password.dart';
 import 'package:quick_shop_app/features/authentication/screens/signup/signup.dart';
 import 'package:quick_shop_app/utils/constants/sizes.dart';
@@ -63,7 +64,7 @@ class CustomLoginForm extends StatelessWidget {
             SizedBox(
               width: double.infinity,
               child: ElevatedButton(
-                onPressed: () {},
+                onPressed: () => Get.to(() => const NavigationMenu()),
                 child: const Text(CustomTextStrings.signIn),
               ),
             ),
