2b29f72a877c8d4e8c09c968873228343ac0c522
Adebayo Bakare <hardebayobakare@gmail.com>
2024-11-14 18:59:01 -0500

    firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:quick_shop_app/app.dart';
+import 'package:quick_shop_app/firebase_options.dart';
 
 
-void main() {
+Future<void> main() async{
 
   // Todo: Add Widgets Biding
   // Todo: Init Local Storage
   // Todo: Await Native Splase
-  // Todo: Init Firebase
+  
+  // initialize Firebase
+  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then(
+  //   (FirebaseApp firebaseApp) => Get.put(AuthenticationRepository())
+  // );
+  
   // Todo: Init Authentication
 
   
